0828a31e2714cf1e414a773df6a875b6a9020b4a
youzhen <yl301114@gmail.com>
2023-08-12 16:03:57 +0800

    header

---
+++ public/js/common.js
@@ -0,0 +1,26 @@
+$(".header_lang_icon").click(function(){
+    $(this).toggleClass("active");
+    $(this).parents(".header_lang").find(".header_lang_list").fadeToggle(300);
+})
+
+$(document).click(function (event) {
+    var lang = $(".header_lang");
+    if (!lang.is(event.target) && lang.has(event.target).length === 0) {
+        $(".header_lang_icon").removeClass("active");
+        $(".header_lang_list").fadeOut(300);
+    }
+});
+
+$(".header_search_icon").click(function(){
+    $(this).parents(".header_search").toggleClass("active");
+})
+
+$(".header_menu").click(function(){
+    $(this).toggleClass("active");
+    $(".header_nav").fadeToggle(300);
+})
+
+$(".header_nav_first>li.drop").click(function(){
+    $(this).toggleClass("active");
+    $(this).find(".header_nav_second").slideToggle(300);
+})
